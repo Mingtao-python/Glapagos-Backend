2f1103a3f102da1037f375ec6fe58904b94c3832
Your Name <you@example.com>
2026-05-14 08:04:03 -0400

    fix: remove drf_yasg import causing 500 on all routes

--- api/config/urls.py
+++ api/config/urls.py
@@ -1,35 +1,12 @@
 """Main URLs module."""
 
-# Admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-
-# Django
 from django.urls import include, path, re_path
 
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="{{app_name}} API Protocol",
-        default_version="v1",
-        description="{{app_name}} API Protocol",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 urlpatterns = [
-    # Django Admin
     path(settings.ADMIN_URL, admin.site.urls),
-    path(
-        "api/v1/docs/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
     path("i18n/", include("django.conf.urls.i18n")),
     re_path(settings.API_URI + "/", include("api.users.urls")),
     re_path(settings.API_URI + "/", include("api.events.urls")),
